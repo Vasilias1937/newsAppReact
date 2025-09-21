1220be3795af978c0ec73b4dfb113f53992e25d1
Ubermensch <ubermensch1937@bk.ru>
2025-09-21 18:40:47 +0300

    Add Categories filter, add params object for fetchNews, add Categories Component

--- src/API/apiNews.js
+++ src/API/apiNews.js
@@ -3,13 +3,14 @@ import axios from "axios"
 const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL
 const API_KEY= import.meta.env.VITE_NEWS_API_KEY
 
-export const getNews = async (page_number = 1, page_size = 10) => {
+export const getNews = async ({page_number = 1, page_size = 10, category}) => {
     try {
         const response = await axios.get('/api/search', {
             params: {
               apiKey: API_KEY,
               page_number,
-              page_size
+              page_size,
+              category
             }
           })
 
@@ -17,4 +18,19 @@ export const getNews = async (page_number = 1, page_size = 10) => {
     } catch (error) {
         console.log(error);
     }
+}
+
+
+export const getCategories = async () => {
+  try {
+      const response = await axios.get('/api/available/categories', {
+          params: {
+            apiKey: API_KEY,
+          }
+        })
+
+      return response.data
+  } catch (error) {
+      console.log(error);
+  }
 }
