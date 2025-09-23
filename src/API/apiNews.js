8e16cce6d64819fb843b642872e6287650c9c833
Ubermensch <ubermensch1937@bk.ru>
2025-09-23 18:50:01 +0300

    add keywords search, add useDebounce hook

--- src/API/apiNews.js
+++ src/API/apiNews.js
@@ -3,14 +3,15 @@ import axios from "axios"
 const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL
 const API_KEY= import.meta.env.VITE_NEWS_API_KEY
 
-export const getNews = async ({page_number = 1, page_size = 10, category}) => {
+export const getNews = async ({page_number = 1, page_size = 10, category, find_by_keywords}) => {
     try {
         const response = await axios.get('/api/search', {
             params: {
               apiKey: API_KEY,
               page_number,
               page_size,
-              category
+              category,
+              find_by_keywords
             }
           })
 
